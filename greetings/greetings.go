277a4615f9329798f00a45ca74cb2334e7128f1c
emzem <em.zemskova@gmail.com>
2024-10-21 20:59:08 +0300

    Error checking

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -9,7 +9,7 @@ import (
 func Hello(name string) (string, error) {
 	//If incorrect name ("empty name" error + empty name)
 	if name == "" {
-		return "", errors.New("empty name")
+		return " ", errors.New("empty name")
 	}
 	// Create a message (string type) + nil error
 	message := fmt.Sprintf("Hi, %v. Welcome to the server!", name)
