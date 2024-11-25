50ad5c17439c5d87fec6b93fa0000f21f6bb5535
emzem <em.zemskova@gmail.com>
2024-11-25 21:58:18 +0300

    UserStat

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,19 +1,15 @@
 package storage
 
 import (
-	"context"
 	"log"
 
-	"github.com/jackc/pgx/v4"
 	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 type storage struct {
 	Gormdb *gorm.DB
-	PgxDB  *pgx.Conn
 }
 
 func Init(connString string) (*storage, error) {
@@ -24,12 +20,5 @@ func Init(connString string) (*storage, error) {
 		log.Fatal("failed to connect to the database:", err)
 		return nil, errors.Wrap(err, "failed to connect to the database")
 	}
-	conn, err := pgx.Connect(context.Background(), connString)
-	if err != nil {
-		log.Fatal("failed to connect to the database:", err)
-		return nil, errors.Wrap(err, "failed to connect to the database")
-	}
-	DB.PgxDB = conn
-	logrus.Info("Database connection established successfully")
 	return DB, nil
 }
